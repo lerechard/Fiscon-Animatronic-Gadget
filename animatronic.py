7fb4fe5ab84527318043ba0ba637c97442a53a75
lerechard <122837658+lerechard@users.noreply.github.com>
2025-07-06 16:08:51 -0700

    Update animatronic.py

--- animatronic.py
+++ animatronic.py
@@ -1,7 +1,18 @@
-from flask import Flask, send_file
+try:
+    import flask
+except ImportError:
+    import subprocess
+    subprocess.check_call(['pip', 'install', 'flask', 'flask-socketio', 'eventlet'])
+
+try:
+    from picamera2 import Picamera2
+except ImportError:
+    subprocess.check_call(['sudo', 'apt', 'install', '-y', 'python3-picamera2'])
+    exit("Re-run the script after installing Picamera2.")
+
+from flask import Flask
 from flask_socketio import SocketIO
 import io
-from picamera2 import Picamera2
 from time import sleep
 
 app = Flask(__name__)
@@ -10,7 +21,7 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 camera = Picamera2()
 camera.configure(camera.create_still_configuration())
 camera.start()
-sleep(2)  # Allow camera to warm up
+sleep(2)
 
 @socketio.on('take_photo')
 def handle_take_photo():
